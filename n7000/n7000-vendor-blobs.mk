67b1a0e4100d283b26f815c887eb909715d14a89
leewp14 <lee.wp14@gmail.com>
2020-05-13 13:28:50 +0800

    n7000: build libsecril-client
    Use open-source implementation of libsecril-client.so.

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -19,7 +19,6 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/proprietary/system/bin/gpsd:system/bin/gpsd \
     $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/vendor/lib/libakm.so \
     $(LOCAL_PATH)/proprietary/system/lib/libsec-ril.so:system/vendor/lib/libsec-ril.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:system/vendor/lib/libsecril-client.so \
     $(LOCAL_PATH)/proprietary/system/lib/hw/gps.manta.so:system/vendor/lib/hw/gps.exynos4.vendor.so
 
 PRODUCT_COPY_FILES += \
